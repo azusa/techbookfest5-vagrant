2c0a2351091a947d3f27a351098f98f0bf6dd3ae
Hiroyuki Onaka <h.onaka@gxp.co.jp>
2018-09-26 15:17:50 +0900

    フォーマット調整

--- provisioning.sh
+++ provisioning.sh
@@ -3,4 +3,5 @@ yum -y install ansible
 CURRENT=$(cd $(dirname $0) && pwd)
 cd $CURRENT
 
-PYTHONUNBUFFERED=1 ANSIBLE_FORCE_COLOR=true ansible-playbook --limit="default" --inventory-file=localhost -v provision/localhost.yml
+PYTHONUNBUFFERED=1 ANSIBLE_FORCE_COLOR=true ansible-playbook \
+  --limit="default" --inventory-file=localhost -v provision/localhost.yml
